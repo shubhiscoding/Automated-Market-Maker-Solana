ff6a0e879ad4bbc57db80ee0017aa0e1cf9dbe9e
shubhiscoding <mygitcoin@gmail.com>
2025-09-24 17:12:00 +0530

    updated UI

--- src/components/AmmLayout.tsx
+++ src/components/AmmLayout.tsx
@@ -11,18 +11,6 @@ export default function AmmLayout({ children }: { children: React.ReactNode }) {
   if (!account || !cluster) {
     return (
       <div className="min-h-screen flex flex-col">
-        {/* Navigation Bar */}
-        <div className="border-b bg-white dark:bg-gray-900">
-          <div className="max-w-6xl mx-auto p-4">
-            <Link href="/">
-              <Button variant="outline" size="sm">
-                <ArrowLeft className="h-4 w-4 mr-2" />
-                Back to Home
-              </Button>
-            </Link>
-          </div>
-        </div>
-
         {/* Main Content */}
         <div className="flex-1 flex flex-col items-center justify-center text-center space-y-8 p-6">
           <div className="space-y-4">
